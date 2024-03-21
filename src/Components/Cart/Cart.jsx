7419db2dc9a4731bc5e3eb2ee9b869415437c492
Sohidul Islam <anonto1080@gmail.com>
2024-03-21 11:21:33 +0600

    side bar of lg deveices

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react'
 
-const Cart = () => {
+const Cart = (props) => {
     const [state,setState] = useState(true);
-
   return (
-    <div>
+    <div className=' w-auto h-[300px] relative'>
         <div
-            className='w-[290px] h-[340px] mb-4 relative mx-5 rounded-lg bg-white overflow-hidden shadow-md drop-shadow-md'
+            className='w-[140px] h-[140px] sm:w-[170px] sm:h-[170px] md:w-[220px] lg:w-[170px] lg:h-[170px] md:h-[220px] mb-4 mr-2 md:mr-5 mt-5 rounded-lg bg-white overflow-hidden shadow-md drop-shadow-md'
         >
-            {
+            {/* {
                 state && <div 
                 onMouseOver ={()=> setState(true)}
                 onMouseLeave={()=> setState(false)}
@@ -19,22 +18,18 @@ const Cart = () => {
                 className='z-[9999] hover:bg-[crimson] hover:scale-125 duration-150  w-[200px] mb-10 h-[40px] bg-[#00a200] text-white absolute mt-[130px] ml-12 font-bold rounded-lg'>SHOP NOW</button>
             </div>
             }
-            
+             */}
 
 
-            <div
-                 onMouseOver ={()=> setState(true)}
-                 onMouseLeave={()=> setState(false)}
-            
-            className='hover:blur-sm w-[290px] h-[290px]'>
+                {/* //  onMouseOver ={()=> setState(true)}
+                //  onMouseLeave={()=> setState(false)} */}
+
                 <img src="" className='w-full h-full' />
-            </div>
-            <h2 className='p-2 text-xl font-[450]'>
-                {"Mailer Boxes"}
-            </h2>
-
             
         </div>
+            <h2 className='p-1 font-semibold absolute m-[3px] text-sm md:text-xl'>
+                {'title'}
+            </h2>
     </div>
   )
 }
--- src/Components/Downer of the hero/HeroDown.jsx
+++ src/Components/Downer of the hero/HeroDown.jsx
@@ -1,11 +1,384 @@
 import React from 'react'
-import TorSellers from '../TopsellersCart/TorSellers'
+import { CiMap, CiSettings } from 'react-icons/ci'
+import { FaChevronRight, FaIdCard, FaIdCardAlt } from 'react-icons/fa'
+import { PiPackageLight, PiSliders, PiSmileySticker, PiTShirtLight } from "react-icons/pi";
+import { MdAddCircleOutline, MdInsertPageBreak, MdOutlineKeyboardArrowRight, MdOutlineShoppingBag } from "react-icons/md"
+import { GiKnightBanner } from "react-icons/gi";
+import { FiBox } from "react-icons/fi";
+import { BsCardList } from 'react-icons/bs';
+import Cart from '../Cart/Cart';
 
 const HeroDown = () => {
+  var i = {
+    imgurl:"imgUrl",
+    title:"Title"
+  }
   return (
-    <div className='h-[100vh] xl:px-28 w-full mt-8 flex justify-center'>
-        <div className=' lg:w-[25%] mr-6 bg-slate-200 h-[25%]'></div>
-        <div className='w-[100%] lg:w-[75%] h-[50%] bg-orange-400'>
+    <div className='h-auto md:px-10 xl:px-28 w-full mt-8 flex justify-center'>
+        <div className='h-full hidden shadow-lg shadow-[#100f0f58] lg:block border border-[#E1E1E1] lg:w-[25%] mr-6 bg-[#ffff]'>
+              <div className='bg-[#F8F7F5] w-full h-14 flex justify-start pl-5 items-center text-black text-xl font-semibold mb-5'>
+                Popular Products
+              </div>
+              <div className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4'>
+                <CiSettings className='text-2xl text-[#9F9F9F]'/>
+                <p>Custom Product Builder</p>
+              </div>
+              
+              <div className=''>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 relative' href="">
+                  <PiTShirtLight className='text-2xl text-[#9F9F9F] mr-1'/>
+                  <p>Apparel</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 relative' href="">
+                  <GiKnightBanner className='text-2xl text-[#9F9F9F] mr-1'/>
+                  <p>Banner</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 relative' href="">
+                  <FiBox className='text-2xl text-[#9F9F9F] mr-1'/>
+                  <p>Boxes</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 relative' href="">
+                  <CiMap className='text-2xl text-[#9F9F9F] mr-1'/>
+                  <p>Brochures</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 relative' href="">
+                  <FaIdCard className='text-xl text-[#9F9F9F] mr-1'/>
+                  <p>Business Cards</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 relative' href="">
+                  <BsCardList className='text-2xl text-[#9F9F9F] mr-1'/>
+                  <p>Flyers</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 relative' href="">
+                  <MdInsertPageBreak className='text-2xl text-[#9F9F9F] mr-1'/>
+                  <p>Labels</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 relative' href="">
+                  <PiPackageLight className='text-2xl text-[#9F9F9F] mr-1'/>
+                  <p>Packaging</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 relative' href="">
+                  <FaIdCardAlt className='text-xl text-[#9F9F9F] mr-1'/>
+                  <p>Postcards</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 relative' href="">
+                  <MdOutlineShoppingBag className='text-2xl text-[#9F9F9F] mr-1'/>
+                  <p>Promotional Products</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 relative' href="">
+                  <PiSliders className='text-2xl text-[#9F9F9F] mr-1'/>
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] border-b border-[#9F9F9F] flex justify-start items-center pl-5 pb-4 relative' href="">
+                  <PiSmileySticker className='text-2xl text-[#9F9F9F] mr-1'/>
+                  <p>Stickers</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+              <div>
+                <a className='text-[15px hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt- relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+
+
+              <div>
+                <a className='text-[15px] hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-4 mt-4 relative' href="">
+                  <p>Signs</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+              {/* last component */}
+
+              <div>
+                <a className='text-[15px] font-bold hover:bg-[#EBF1F1] flex justify-start items-center pl-5 mb-8 mt-4 relative' href="">
+                  <p>See More Products</p>
+                  <MdOutlineKeyboardArrowRight className='right-4 text-xl absolute' />
+                </a>
+              </div>
+
+
+        </div>
+        <div className='w-[100%] p-[4%] md:p-6 lg:w-[75%] h-[50vh]'>
+          <h1 className='text-xl sm:text-2xl md:text-3xl font-bold'>Top Collections</h1>
+          <div className='flex overflow-auto'>
+            <Cart />
+            <Cart />
+            <Cart />
+          </div>
         </div>
     </div>
   )
