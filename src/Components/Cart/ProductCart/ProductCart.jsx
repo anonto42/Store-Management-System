810aa33849acee65e11a8a5ace3149d567fc5c52
Sohidul Islam <anonto1080@gmail.com>
2024-04-01 20:06:32 +0600

    push new elements

--- src/Components/Cart/ProductCart/ProductCart.jsx
+++ src/Components/Cart/ProductCart/ProductCart.jsx
@@ -2,13 +2,72 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 const ProductCart = ({item}) => {
+  console.log(item.reating)
+  
   return (
     <Link to='/product/xyz'>
-        <div className=' w-[300px] h-[150px] border-2 border-white'>
-            <img className='' src={''} />
-            <div className=''>
-                <h3 className=''>{'this is the title'}</h3>
-                <h3 className=''>{'item.price'}</h3>
+        <div className=' w-[460px] sm:w-[540px] md:w-[700px] mt-5 h-[350px] lg:mx-6 lg:w-[350px] lg:h-[280px] hover:scale-105 hover:shadow-md duration-100 ease-in border-2 bg-white rounded-lg pb-2'>
+            <img className='w-full h-[75%]' src={item.img} />
+            <div className='flex justify-between items-center px-5 pt-2'>
+              <div className='tracking-tight leading-6 lg:leading-5'>  
+                  <h3 className='text-[20px] lg:text-[17px]  font-[670] font-serif'>{item.title}</h3>
+                  {/* Rating */}
+
+                  <div className='text-yellow-400 text-[20px] font-[670] font-serif lg:text-[16.2px]'>
+                    {
+                      item.reating == 1? 
+                      <div>
+                        <span>&#9733;</span> 
+                        <span>&#9734;</span> 
+                        <span>&#9734;</span> 
+                        <span>&#9734;</span> 
+                        <span>&#9734;</span> 
+                      </div> : <></> 
+                            && 
+                    item.reating == 2 ? 
+                      <div>
+                        <span>&#9733;</span> 
+                        <span>&#9733;</span> 
+                        <span>&#9734;</span> 
+                        <span>&#9734;</span> 
+                        <span>&#9734;</span> 
+                      </div>  : <></>
+                              &&
+                    item.reating == 3 ?
+                    <div>
+                        <span>&#9733;</span> 
+                        <span>&#9733;</span> 
+                        <span>&#9733;</span> 
+                        <span>&#9734;</span> 
+                        <span>&#9734;</span> 
+                      </div>  : <></>
+                      &&
+                      item.reating == 4 ?
+                      <div>
+                          <span>&#9733;</span> 
+                          <span>&#9733;</span> 
+                          <span>&#9733;</span> 
+                          <span>&#9733;</span> 
+                          <span>&#9734;</span> 
+                        </div>  : <></>
+                      &&
+                      item.reating == 5 ?
+                      <div>
+                          <span>&#9733;</span> 
+                          <span>&#9733;</span> 
+                          <span>&#9733;</span> 
+                          <span>&#9733;</span> 
+                          <span>&#9733;</span> 
+                        </div>  : <></>
+                    }
+                    
+                    
+                    
+                  </div>
+
+                  <h3 className='text-sm font-[670] font-serif'>{'item.price'}</h3>
+              </div>
+              <button>View</button>
             </div>
         </div>
     </Link>
--- src/Pages/Product/Catagory.jsx
+++ src/Pages/Product/Catagory.jsx
@@ -2,8 +2,60 @@ import React from 'react'
 import ProductCart from '../../Components/Cart/ProductCart/ProductCart'
 
 const Catagory = () => {
+  const data =[
+    {
+      img:"https://staticecp.uprinting.com/10796/250x250/DM-4881-01.jpg",
+      title:"Mailer Boxes",
+      reating:2
+    },
+    {
+      img:"https://staticecp.uprinting.com/260/250x250/Vinyl_Banners_Marketing_Materials_A.jpg",
+      title:"Vinyl Banners",
+      reating:2
+    },
+    {
+      img:"https://staticecp.uprinting.com/10910/250x250/Custom_Stickers_Gallery_Badge.jpg",
+      title:"Stickers",
+      reating:5
+    },
+    {
+      img:"https://staticecp.uprinting.com/1812/250x250/Standard_Postcards_Marketing_Materials_B.jpg",
+      title:"Postcards",
+      reating:4
+    },
+    {
+      img:"https://staticecp.uprinting.com/71/250x250/Business_Flyers_Restaurant_Advertising_Materials_A.jpg",
+      title:"Flyers",
+      reating:3
+    },
+    {
+      img:"https://staticecp.uprinting.com/10176/250x250/Brochures_Marketing_Materials_A.webp",
+      title:"Brochures",
+      reating:1
+    },
+    {
+      img:"https://staticecp.uprinting.com/6975/250x250/UP_CTS_Label_PDP_Image_B.jpg",
+      title:"Labels",
+      reating:4
+    },
+    {
+      img:"https://staticecp.uprinting.com/7829/250x250/Apparel-Jackets-J317.png",
+      title:"Jackets",
+      reating:4
+    },
+    {
+      img:"https://staticecp.uprinting.com/6987/250x250/UP_Roll_Stickers_PDP_Image_A.jpg",
+      title:"Roll Stickers",
+      reating:5
+    },
+    {
+      img:"https://staticecp.uprinting.com/1070/250x250/Window_Decals_Small_Busisness_Advertising_Materials_A.jpg",
+      title:"Decals",
+      reating:5
+    }
+  ]
   return (
-    <div className=' w-full bg-[#3333] lg:pt-12 '>
+    <div className=' w-full bg-[#3333] lg:pt-12 pb-8'>
       <div className='w-full p-5 sm:p-7 md:pl-12 bg-gradient-to-r  from-[#003654] vi-[#FEDDB3] to-[#004425]'>
         <h1 className='text-[25px] md:mb-2 sm:text-[28px] md:text-[31px] lg:text-[34px] font-semibold text-white'>
           {"The product catagory name"}
@@ -25,15 +77,11 @@ const Catagory = () => {
           <li className=''>Design your business cards online or upload your own design</li>
         </ul>
       </div>
-      <div className='flex flex-wrap'>
-        <ProductCart />
-        <ProductCart />
-        <ProductCart />
-        <ProductCart />
-        <ProductCart />
-        <ProductCart />
-        <ProductCart />
-        <ProductCart />
+      <div className='flex flex-wrap px-[2px] justify-center'>
+        {
+          data.map((item,index)=><ProductCart item={item} key={index} />)
+        }
+        
       </div>
     </div>
   )
