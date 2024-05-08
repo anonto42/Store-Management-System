9ec5d2368b5cd5c609878b2bd53e7bf32ccb84c8
sohidul Islam Ananto <anontom90@gmail.com>
2024-05-09 00:37:10 +0600

    pacage json

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import { Slider } from 'infinite-react-carousel'
+// import { Slider } from 'infinite-react-carousel'
 
 const Slide = ({children,slidesToShow}) => {
   
   return (
       <div className='px-2 containers'>
-        <Slider slidesToShow={slidesToShow} arrowsScroll={1} >
+        <div slidesToShow={slidesToShow} arrowsScroll={1} >
           {children}
-        </Slider>
+        </div>
       </div>
   )
 }
