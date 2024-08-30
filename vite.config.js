52e68e0573e7daebb8ce9e20e9446845ea9d6ce7
Sohidul Islam Ananto <anontom90@gmail.com>
2024-08-30 16:06:53 +0600

    proxy updated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // server:{
+  //   proxy:{
+  //     '/user': {
+  //       target: 'http://localhost:3000/api/user',
+  //       changeOrigin: true,
+  //       rewrite: path => path.replace(/^\/api/, '')
+  //     }
+  //   }
+  // }
 })
