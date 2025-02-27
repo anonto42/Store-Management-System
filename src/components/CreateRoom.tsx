74e935fff9b67363edc4680a8692cc8305c5faee
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-02-27 22:34:48 +0600

    added the room creating section

---
+++ src/components/CreateRoom.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CreateRoom = () => {
+  return (
+    <section className='w-full'>
+        <div className='mt-24 w-full'>
+            <h1 className='mb-4 text-2xl font-bold'>Room: 1</h1>
+        </div>
+    </section>
+  )
+}
+
+export default CreateRoom
