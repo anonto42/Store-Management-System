9e59b72fcc91a916094bb142c7504bcb2293596c
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-02-27 11:15:45 +0600

    created th eapplicaiton layout

---
+++ src/app/(Home)/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Quiet chat",
+  description: "Quiet-Chat is a secure chat application that never store your messages for your privacy and your all messages was hashed when it was sending to receivers.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#0D1117]`}
+      >
+        <div className="body_container">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
