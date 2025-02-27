8df0a339bdef518aee8aea0febc7fc52fe312193
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-02-27 23:46:16 +0600

    created the message

---
+++ src/components/Message.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+
+interface ChatMessageProps {
+    isSystemMessage: boolean;
+    isOwnerMessge: boolean;
+    message: string;
+    sender: string;
+}
+
+const Message = ( { sender, message, isOwnerMessge }: ChatMessageProps ) => {
+    const isSystemMessage: boolean = sender === "system";
+  return (
+    <section 
+        className={`
+            flex
+            ${
+                isSystemMessage ? 
+                    "justify-center" :
+                        isOwnerMessge ?
+                            "justify-end" :
+                            "justify-start"
+            }
+        `}
+    >
+        <div className={`
+            max-w-[300px] min-w-[50px] py-3 rounded-lg
+            ${
+                isSystemMessage ? 
+                    "bg-gray-800 text-center text-xs" :
+                        isOwnerMessge ?
+                            "bg-blue-500" :
+                            "bg-white text-[#0D1117]"
+            }
+        `}>
+            {
+                !isSystemMessage && (
+                    <p className='text-sm font-medium'>{sender}</p>
+                )
+            }
+            <p>{message}</p>
+        </div>
+    </section>
+  )
+}
+
+export default Message
