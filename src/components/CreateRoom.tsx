89e39dc729624fb4a27752662e90f2491bac93be
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-02-27 23:46:45 +0600

    added the create room

--- src/components/CreateRoom.tsx
+++ src/components/CreateRoom.tsx
@@ -1,11 +1,19 @@
+"use client"
 import React from 'react'
+import ChatForm from './ChatForm'
 
 const CreateRoom = () => {
+
+    const handelSendMessage = ( message : string ) => {
+        console.log(message)
+    }
+
   return (
     <section className='w-full'>
         <div className='mt-24 w-full'>
             <h1 className='mb-4 text-2xl font-bold'>Room: 1</h1>
         </div>
+        <ChatForm onSendMessage={handelSendMessage}/>
     </section>
   )
 }
