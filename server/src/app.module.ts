944abf915ddcb77631c9f43c6d88c894dce3054b
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-04-17 16:40:43 +0600

    main module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:".env",
+      isGlobal: true
+    }),
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI as string
+    ),
+    UsersModule
+  ]
 })
 export class AppModule {}
