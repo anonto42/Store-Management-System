af269d527afa8290eef35386af657e23f33946bf
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-04-26 15:45:57 +0600

    Allroutes are protected with the context

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,12 +1,14 @@
 import TabBar from "@/components/TabBar";
-import { Tabs } from "expo-router";
+import { useAuth } from "@/lib/AuthContext";
+import { Redirect, Tabs } from "expo-router";
  
 export default () => {
-    return(
+    const { user } = useAuth();
+    return user ? (
         <Tabs tabBar={ props => <TabBar {...props} />}>
             <Tabs.Screen name="index" options={{ title:"Home", headerShown: false }} />
             <Tabs.Screen name="info" options={{ title:"Info", headerShown: false }} />
             <Tabs.Screen name="profile" options={{ title:"Settings",headerShown: false }} />
         </Tabs>
-    )
+    ): <Redirect href={"/auth"} />
 }
