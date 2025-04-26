ed50d43a3833e9c7ab4a5b0761212449341012a1
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-04-26 16:15:52 +0600

    modified

--- mobile/app/auth.tsx
+++ mobile/app/auth.tsx
@@ -4,15 +4,15 @@ import React, { useState } from 'react';
 import { View, TextInput, Text, Pressable } from 'react-native';
 
 export default function LoginScreen() {
-  const [email, setEmail] = useState('');
+  const [username, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { signin } = myContext();
 
   return (
     <View style={styleSheet.main_bg}>
       <View style={styleSheet.auth_container}>
-        <Text style={styleSheet.auth_header}>Email:</Text>
-        <TextInput style={styleSheet.auth_inp} onChangeText={setEmail} value={email} placeholder="Write hear" />
+        <Text style={styleSheet.auth_header}>Username:</Text>
+        <TextInput style={styleSheet.auth_inp} onChangeText={setEmail} value={username} placeholder="Write hear" />
         <Text style={styleSheet.auth_header}>Password:</Text>
         <TextInput
           placeholder="Write hear"
@@ -21,7 +21,7 @@ export default function LoginScreen() {
           onChangeText={setPassword}
           value={password}
         />
-        <Pressable style={styleSheet.auth_button} onPress={()=> signin(email, password)}>
+        <Pressable style={styleSheet.auth_button} onPress={()=> signin({username, password})}>
           <Text style={styleSheet.auth_button_text}>Login</Text>
         </Pressable>
       </View>
