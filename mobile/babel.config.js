8264f660fcc68a55f2156c34eef40649d84bd2c8
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-04-27 16:21:03 +0600

    some code modified

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,17 +1,10 @@
-module.exports = function (api) {
-    api.cache(true);
-    return {
+module.exports = {
       presets: ['babel-preset-expo'],
       plugins: [
         ['module:react-native-dotenv', {
           moduleName: '@env',
           path: '.env',
-          blacklist: null,
-          whitelist: null,
-          safe: false,
-          allowUndefined: true,
         }],
       ],
     };
-  };
   
