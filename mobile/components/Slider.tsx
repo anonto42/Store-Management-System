93815c717df4da915c2eaa7f2814d15d99ae93ca
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-04-27 16:45:52 +0600

    slider body

---
+++ mobile/components/Slider.tsx
@@ -0,0 +1,27 @@
+import { View, Text, ScrollView } from 'react-native';
+import React from 'react';
+import Card from './Card';
+import { styleSheet } from '@/constants/styling';
+
+const Slider = ({title, data}:{ title: string, data: any[] }) => {
+
+  return (
+    <View style={styleSheet.slider_bg}>
+      <Text style={styleSheet.slider_heading} >{title.toUpperCase()}</Text>
+      <ScrollView
+        showsHorizontalScrollIndicator={false}
+        horizontal={true}
+      >
+        {
+          data.map( ( element, index ) => {
+            return (
+              <Card data={element} key={index} />
+            )
+          })
+        }
+      </ScrollView>
+    </View>
+  )
+}
+
+export default Slider
