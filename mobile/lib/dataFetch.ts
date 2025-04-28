2e1462348a84edd2fef7608494671bc62364c88f
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-04-28 19:36:22 +0600

    Dynamic data fetch

---
+++ mobile/lib/dataFetch.ts
@@ -0,0 +1,20 @@
+import { EXPO_API_URL } from "@env";
+import axios from "axios"
+
+type input = "sales" | "due" | "payment" | "product" | "user"
+
+export const salesData = async (props: input) => {
+    try {
+
+        const { data } = await axios.get(`${EXPO_API_URL}/user/admin/${props}`,{});
+        if (!data) {
+            throw new Error(`Con't get the data of the ${props}`)
+        }
+
+        return data.data.data
+        
+    } catch (error) {
+        console.log(error)
+        throw new Error(`This error is from the data fatching`)    
+    }
+}
