4a0268df19b77a2ed61318ca8208c2982d7f98f0
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-04-29 02:06:16 +0600

    styling

--- mobile/constants/styling.ts
+++ mobile/constants/styling.ts
@@ -21,6 +21,11 @@ export const styleSheet = StyleSheet.create({
       fontWeight: 500,
       marginBottom: 5
     },
+    scrollView_style:{
+        minHeight: "100%",
+        paddingBottom: 10,
+        gap: 40
+      },
     auth_inp:{
       backgroundColor: "gray",
       fontSize: 15,
@@ -49,5 +54,100 @@ export const styleSheet = StyleSheet.create({
         height: 80,
         backgroundColor: "blue",
         borderRadius: 100
+    },
+    slider_bg:{
+      height: "auto",
+      width: "100%",
+      backgroundColor: "rgba(199, 211, 212, 0.2);",
+      paddingTop: 15,
+      paddingHorizontal: 10
+    },
+    slider_heading:{
+      color: "white",
+      fontSize: 25,
+      borderBottomWidth: 2,
+      borderStyle: "dotted",
+      borderColor: "#868787",
+      paddingBottom: 10
+    },
+    card_box:{
+      width: 150,
+      height: 100,
+      backgroundColor: "white",
+      marginVertical: 10,
+      marginRight: 10,
+      flex: 1,
+      padding: 5,
+      borderRadius: 5,
+      display: "flex",
+      alignItems:"flex-start",
+      gap: 1.5
+    },
+    cart_buyer_name:{
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    cart_buyer_name_bold:{
+      fontWeight: 600,
+      marginRight: 5,
+      fontSize: 15
+    },
+    cart_buyer_name_inner:{
+      fontStyle: "italic"
+    },
+    add_update_box_wraper:{
+      width: "100%",
+      minHeight: 400,
+      padding: 15
+    },
+    add_update_box:{
+      backgroundColor: "#ffff",
+      width: "100%",
+      minHeight: 100,
+      borderRadius: 10,
+      overflow: "hidden"
+    },
+    add_update_box_text:{
+      fontSize: 20,
+      fontWeight: 500,
+      borderBottomWidth: 1,
+      padding: 10,
+      borderBottomColor: "#969696"
+    },
+    add_update_box_underline:{
+      borderBottomWidth:1,
+      borderBottomColor: "#969696"
+    },
+    add_update_box_details:{
+      padding: 10
+    },
+    add_update_box_text_details:{
+      fontSize:20,
+      fontWeight: 500,
+      // marginBottom:10
+    },
+    add_update_box_text_inp_box:{
+      marginTop: 10,
+      gap: 10
+    },
+    add_update_box_text_inp:{
+      fontWeight: 500,
+      borderColor: "#969696",
+      borderWidth: 1,
+      paddingHorizontal: 10,
+      borderRadius: 10
+    },
+    add_update_box_button:{
+      width: "100%",
+      height: 40,
+      backgroundColor: "#52A8FF"
+    },
+    add_update_box_button_text:{
+      margin:"auto",
+      fontSize: 18,
+      fontWeight: "600",
+      color:"#ffff"
     }
   })
