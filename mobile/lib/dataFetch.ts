7d4a8a14d302f536192b10def8c03b1f8a4a5967
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-04-28 20:37:08 +0600

    added the jwt cookie named my store

--- mobile/lib/dataFetch.ts
+++ mobile/lib/dataFetch.ts
@@ -1,12 +1,19 @@
 import { EXPO_API_URL } from "@env";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios"
 
-export type input = "sales" | "due" | "payment" | "product" | "user"
+export type input = "sales" | "due" | "payment" | "product" | "user";
 
 export const salesData = async (props: input) => {
     try {
-
-        const { data } = await axios.get(`${EXPO_API_URL}/user/admin/${props}`,{});
+        const token = await AsyncStorage.getItem('sms');
+        
+        const { data } = await axios.get(`${EXPO_API_URL}/user/admin/${props}`,{
+            withCredentials: true,
+            headers: {
+                Cookie: `MyStore=${token}`,
+            }
+        });
         if (!data) {
             throw new Error(`Con't get the data of the ${props}`)
         }
